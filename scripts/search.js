30f3c4e21ccf51ec45e10ca15bc863c56b226b22
NatSch45 <nathan.schneider4505@gmail.com>
2021-04-06 09:31:34 +0200

    Recherche en temps réel effective

--- scripts/search.js
+++ scripts/search.js
@@ -1,6 +1,63 @@
 console.log("Script search loaded")
 
-document.getElementById('searchInput').onkeydown = function(e){
-    let text = this.value
-    console.log(text)
+let search = ""
+
+document.getElementById('searchInput').addEventListener('keyup', () => {
+    search = document.getElementById('searchInput').value
+    callJson()
+})
+
+function loadTab(heroes) {
+    console.log(search)
+    let lines = document.getElementsByClassName('line')
+    for (let line of Array.from(lines)) {
+        line.remove()
+    }
+
+    for (let i in heroes) {
+        let hero = heroes[i]
+        if (search != "") {
+            if (hero.biography.fullName.includes(search)) {
+                makeTab(hero)
+            }
+        } else {
+            makeTab(hero)
+        }
+    }
+}
+
+callJson()
+
+
+
+function callJson() {
+    fetch('https://rawcdn.githack.com/akabab/superhero-api/0.2.0/api/all.json')
+    .then((response) => response.json()) // parse the response from JSON
+    .then(loadTab) // .then will call the function with the JSON value
+}
+
+function makeTab(hero) {
+    let tab = document.getElementById('elements')
+    let tr = document.createElement('tr')
+    tr.classList.add('line')
+
+    // Colonnes
+    let tdId = document.createElement('td')
+    let tdName = document.createElement('td')
+    let tdFullName = document.createElement('td')
+
+    // Contenus
+    tdId.appendChild(document.createTextNode(hero.id))
+    tdName.appendChild(document.createTextNode(hero.name))
+    tdFullName.appendChild(document.createTextNode(hero.biography.fullName))
+    tr.appendChild(tdId); tr.appendChild(tdName); tr.appendChild(tdFullName)
+
+    let powerStats = hero['powerstats']
+    for (let i in powerStats) {
+        let td = document.createElement('td')
+        td.appendChild(document.createTextNode(powerStats[i]))
+        tr.appendChild(td)
+    }
+
+    tab.appendChild(tr)
 }
