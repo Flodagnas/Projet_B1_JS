3b2c68bf506b5c309ba6b17e66429e0f98eb6d60
NatSch45 <nathan.schneider4505@gmail.com>
2021-04-08 01:32:45 +0200

    tri de taille

--- scripts/fusion.js
+++ scripts/fusion.js
@@ -104,6 +104,13 @@ document.getElementById('weight').addEventListener('click', () => { sort('weight
 document.getElementById('placeOfBirth').addEventListener('click', () => { sort('placeOfBirth', 13) })
 document.getElementById('alignement').addEventListener('click', () => { sort('alignement', 14) })
 
+document.getElementById('intelligence').addEventListener('click', () => { sort('intelligence', 3) })
+document.getElementById('strength').addEventListener('click', () => { sort('strength', 4) })
+document.getElementById('speed').addEventListener('click', () => { sort('speed', 5) })
+document.getElementById('durability').addEventListener('click', () => { sort('durability', 6) })
+document.getElementById('power').addEventListener('click', () => { sort('power', 7) })
+document.getElementById('combat').addEventListener('click', () => { sort('combat', 8) })
+
 function sort(category, nbr) {
     deleteLines()
     let numberValue = ['intelligence', 'strength', 'speed', 'durability', 'power', 'combat']
@@ -111,19 +118,53 @@ function sort(category, nbr) {
     
     document.getElementById(category).classList.toggle('notReverse')
     if (document.getElementById(category).className == 'notReverse'){
+
         linesInTab.sort(function(a,b){
-            if (!numberValue.includes(category)) {
-                return a.getElementsByTagName('td')[nbr].innerHTML.localeCompare(b.getElementsByTagName('td')[nbr].innerHTML)
-            } else {
+            if (numberValue.includes(category)) {
+
                 return a.getElementsByTagName('td')[nbr].innerHTML - b.getElementsByTagName('td')[nbr].innerHTML
+
+            } else if (specialValue.includes(category)) {
+
+                if (category == 'height') {
+                    let heightAUnit = a.getElementsByTagName('td')[nbr].innerHTML.split(",")[1]
+                    let heightBUnit = b.getElementsByTagName('td')[nbr].innerHTML.split(",")[1]
+                    let heightAVal = heightAUnit.split(' ')[1] == 'cm' ? parseFloat(heightAUnit.split(' ')[0]) : parseFloat(heightAUnit.split(' ')[0]) * 100.0
+                    let heightBVal = heightBUnit.split(' ')[1] == 'cm' ? parseFloat(heightBUnit.split(' ')[0]) : parseFloat(heightBUnit.split(' ')[0]) * 100.0
+                    return heightAVal - heightBVal
+
+                } else {
+                    console.log(a.getElementsByTagName('td')[nbr].innerHTML)
+                }
+
+            } else {
+
+                return a.getElementsByTagName('td')[nbr].innerHTML.localeCompare(b.getElementsByTagName('td')[nbr].innerHTML)
             }
         })
+
     } else {
         linesInTab.sort(function(a,b){
-            if (!numberValue.includes(category)) {
-                return b.getElementsByTagName('td')[nbr].innerHTML.localeCompare(a.getElementsByTagName('td')[nbr].innerHTML)
-            } else {
+            if (numberValue.includes(category)) {
+
                 return b.getElementsByTagName('td')[nbr].innerHTML - a.getElementsByTagName('td')[nbr].innerHTML
+
+            } else if (specialValue.includes(category)) {
+
+                if (category == 'height') {
+
+                    let heightAUnit = a.getElementsByTagName('td')[nbr].innerHTML.split(",")[1]
+                    let heightBUnit = b.getElementsByTagName('td')[nbr].innerHTML.split(",")[1]
+                    let heightAVal = heightAUnit.split(' ')[1] == 'cm' ? parseFloat(heightAUnit.split(' ')[0]) : parseFloat(heightAUnit.split(' ')[0]) * 100.0
+                    let heightBVal = heightBUnit.split(' ')[1] == 'cm' ? parseFloat(heightBUnit.split(' ')[0]) : parseFloat(heightBUnit.split(' ')[0]) * 100.0
+                    return heightBVal - heightAVal
+
+                } else {
+                    console.log("category : weight")
+                }
+
+            } else {
+                return b.getElementsByTagName('td')[nbr].innerHTML.localeCompare(a.getElementsByTagName('td')[nbr].innerHTML)
             }
         })
     }
